2f29fb5896dc66d994a7a723074a077226f7de27
keon kang <kangkk9714@gmail.com>
2017-12-20 07:25:02 +0000

    V.0.05

---
+++ Assets/Scripts/MM_GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MM_GameManager : MonoBehaviour
+{
+
+
+    public void StartScene()//시작화면씬 시작
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LevelScene()//레벨씬 시작
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void EasyScene()//쉬움씬 시작
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void NormalScene()//쉬움씬 시작
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void HardScene()//어려움씬 시작
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void HowToPlayScene()//튜토리얼씬 시작
+    {
+        SceneManager.LoadScene(5);
+    }
+
+
+    
+}
