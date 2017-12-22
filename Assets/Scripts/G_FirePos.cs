dce747102ee7c05e2b43b093ed19bf137b82582c
keon kang <kangkk9714@gmail.com>
2017-12-22 04:54:06 +0000

    V.0.14

--- Assets/Scripts/G_FirePos.cs
+++ Assets/Scripts/G_FirePos.cs
@@ -8,26 +8,29 @@ public class G_FirePos : MonoBehaviour
     public Transform G_FirePosition;//발사위치
     private int G_Bullet = 6;//6발
     private bool G_isReady = false;//총을 쏠수 있는가?
+
+    //애니메이션
+
     
 
     // Use this for initialization
     void Start ()
     {
-        G_isReady = false;//발사 불가능
+        G_isReady = true;//발사 가능
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if(Input.GetKeyDown(KeyCode.Mouse0)&&G_isReady==true)//마우스버튼 클릭시
+		if(Input.GetKeyDown(KeyCode.Mouse0)&&G_isReady==true)//마우스버튼 클릭시 발포성공
         {
             G_Fire();
         }
-        else
+        if (Input.GetKeyDown(KeyCode.Mouse0) && G_isReady != true)//마우스버튼 클릭시 발포 실패
         {
             G_FireF();
         }
-	}
+    }
 
     void G_Fire()//발사
     {
@@ -51,16 +54,7 @@ public class G_FirePos : MonoBehaviour
                 //몬스터에 대미지 입히는 함수
                 hit.collider.gameObject.SendMessage("OnAttack", SendMessageOptions.DontRequireReceiver);
             }
-
-            if (hit.collider.tag == "Player")//자신 탐지시
-            {
-                object[] _params = new object[2];//레이피격시 내부 정보추출
-                _params[0] = hit.point;
-                _params[1] = 100;
-                //몬스터에 대미지 입히는 함수
-                hit.collider.gameObject.SendMessage("OnAttack", SendMessageOptions.DontRequireReceiver);
-            }
-
+            
             if (hit.collider.tag == "SB")//시작 버튼 탐지시
             {
                 //씬호출
@@ -98,14 +92,17 @@ public class G_FirePos : MonoBehaviour
 
         }
         G_Bullet--;
-        if(G_Bullet<=0)
-        {
-            G_isReady = false;
-        }
+        G_isReady = false;
     }
     void G_FireF()//총발사 실패
     {
 
         //오디오
+    }
+
+    void G_Reload()//재장전
+    {
+
+        G_isReady = true;
     }
 }
