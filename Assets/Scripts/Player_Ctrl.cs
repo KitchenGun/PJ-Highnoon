ed5b0481e7cb5209f476e5e43e1bf4e78718d698
김영조 <kyj970803@daum.net>
2017-12-28 00:44:31 +0000

    v.0.24

--- Assets/Scripts/Player_Ctrl.cs
+++ Assets/Scripts/Player_Ctrl.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Ctrl : MonoBehaviour {
+
+    private int P_HP = 100;
    
     
     //플레이어 피격
@@ -11,21 +13,21 @@ public class Player_Ctrl : MonoBehaviour {
         Debug.Log(string.Format("Hit ray {0} : {1}", _params[0], _params[1]));
 
         //맞은 총알의 데미지를 추출해 플레이어 체력 차감
-        E1_hp -= (int)_params[1];
-        if (E1_hp <= 0)
+        P_HP -= (int)_params[1];
+        if (P_HP <= 0)
         {
             P_Die();
         }
 
         //IsHit트리거 발생
-        animator.SetTrigger("isHit");
+        //animator.SetTrigger("isHit");
     }
 
     void P_Die()
     {
             StopAllCoroutines();
             //플레이어 사망시 적 캐릭터 애니메이션 실행
-            animator.SetBool("IsPDie", true);
+            //animator.SetBool("IsPDie", true);
 
     }
 }
