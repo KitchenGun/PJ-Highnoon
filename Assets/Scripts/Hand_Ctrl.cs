beb830b504ae2bd1eacc3d3cf20a5d52451ee4fb
keon kang <kangkk9714@gmail.com>
2018-01-01 08:36:16 +0000

    V.0.36

--- Assets/Scripts/Hand_Ctrl.cs
+++ Assets/Scripts/Hand_Ctrl.cs
@@ -2,35 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class G_FirePos : MonoBehaviour
+public class Hand_Ctrl : MonoBehaviour
 {
-
     public Transform G_FirePosition;//발사위치
-    private int G_Bullet = 6;//6발
+    public GameObject HandnGun;//총없는손
+    public GameObject HandGun;//총있는손
+    public GameObject HandCurr;//현재 손
+    private bool G_isGrap=false;//총 잡았는가?
     private bool G_isReady = false;//총을 쏠수 있는가?
-    private bool G_isDrop = false;//총이 땅에 떨어졌는가?
-    private Vector3 G_originalPos;//총의 위치
-    private bool G_isGrap;//총 잡았는가?
-    //애니메이션
 
 
 
-    private void Awake()
+    private void Start()
     {
-        G_originalPos = this.gameObject.transform.position;//총의 초기위치 지정    
+        G_isGrap = false;
+        G_isReady = false;
     }
 
-    // Use this for initialization
-    void Start ()
+    void Update()
     {
-        G_isReady = true;//발사 가능
-    }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        Debug.DrawRay(G_FirePosition.position, Vector3.forward*100, Color.green);
-        if (Input.GetKeyDown(KeyCode.Mouse0)&&G_isReady==true)//마우스버튼 클릭시 발포성공
+        Debug.DrawRay(G_FirePosition.position, Vector3.forward * 100, Color.green);
+        if (Input.GetKeyDown(KeyCode.Mouse0) && G_isReady == true)//마우스버튼 클릭시 발포성공
         {
             G_Fire();
             Debug.Log("fire");
@@ -39,13 +31,12 @@ public class G_FirePos : MonoBehaviour
         {
             G_FireF();
         }
-        if(Input.GetKeyDown(KeyCode.R)&&G_isReady==false)
+        if (Input.GetKeyDown(KeyCode.R) && G_isReady == false)
         {
             G_Reload();
         }
 
     }
-
     void G_Fire()//발사
     {
         RaycastHit hit;//레이케스트라인 안에 들어온 물체 변수
@@ -68,7 +59,7 @@ public class G_FirePos : MonoBehaviour
                 //몬스터에 대미지 입히는 함수
                 hit.collider.gameObject.SendMessage("OnAttack", SendMessageOptions.DontRequireReceiver);
             }
-            
+
             if (hit.collider.name == "SB")//시작 버튼 탐지시
             {
                 Debug.Log("hit");
@@ -121,16 +112,17 @@ public class G_FirePos : MonoBehaviour
         G_isReady = true;
     }
 
-    private void OnCollisionEnter(Collision collision)//충돌처리
+    private void OnCollisionEnter(Collision collision)//손에 충돌시
     {
-        if(collision.gameObject.tag == "Ground")
+        if(collision.gameObject.tag=="Gun")
         {
-            this.gameObject.transform.position = G_originalPos;
-        }
-
-        if (collision.gameObject.tag == "P_hand")
-        {
-            this.gameObject
+            if(HandCurr==HandnGun)
+            {
+                HandCurr = HandGun;
+                G_isGrap = true;
+                G_isReady = true;
+            }
         }
     }
+
 }
