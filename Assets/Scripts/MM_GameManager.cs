386c0de249e0060a68f4033d74b4f1b9e7e7e55c
keon kang <kangkk9714@gmail.com>
2018-01-03 06:12:23 +0000

    V.0.44

--- Assets/Scripts/MM_GameManager.cs
+++ Assets/Scripts/MM_GameManager.cs
@@ -5,39 +5,88 @@ using UnityEngine.SceneManagement;
 
 public class MM_GameManager : MonoBehaviour
 {
-    public MeshRenderer a;
+    public MeshRenderer a; //버튼이 되는병
+    public GameObject B_Piece;//병 조각
 
-    public void StartScene()//시작화면씬 시작
+    public void StartScene()
+    { 
+        a.enabled = true;
+        Instantiate(B_Piece, this.transform.position, this.transform.rotation);
+        Invoke("StartScene1", 2.0f);
+    }
+
+    public void LevelScene()
+    {
+        a.enabled = true;
+        Instantiate(B_Piece, this.transform.position, this.transform.rotation);
+        Invoke("LevelScene1", 2.0f);
+    }
+
+    public void EasyScene()
+    {
+        a.enabled = true;
+        Instantiate(B_Piece, this.transform.position, this.transform.rotation);
+        Invoke("EasyScene1", 2.0f);
+    }
+
+    public void NormalScene()
+    {
+        a.enabled = true;
+        Instantiate(B_Piece, this.transform.position, this.transform.rotation);
+        Invoke("NormalScene1", 2.0f);
+    }
+
+    public void HardScene()
+    {
+        a.enabled = true;
+        Instantiate(B_Piece, this.transform.position, this.transform.rotation);
+        Invoke("HardScene1", 2.0f);
+    }
+
+    public void HowToPlayScene()
+    {
+        a.enabled = true;
+        Instantiate(B_Piece, this.transform.position, this.transform.rotation);
+        Invoke("HowToPlayScene1", 2.0f);
+    }
+    
+    public void ExitScene()
+    {
+        a.enabled = true;
+        Instantiate(B_Piece, this.transform.position, this.transform.rotation);
+        Invoke("ExitScene1", 2.0f);
+    }
+    public void StartScene1()//시작화면씬 시작
     {
         SceneManager.LoadScene(0);
     }
 
-    public void LevelScene()//레벨씬 시작
+    public void LevelScene1()//레벨씬 시작
     {
         SceneManager.LoadScene(1);
     }
 
-    public void EasyScene()//쉬움씬 시작
+    public void EasyScene1()//쉬움씬 시작
     {
         SceneManager.LoadScene(2);
     }
 
-    public void NormalScene()//쉬움씬 시작
+    public void NormalScene1()//쉬움씬 시작
     {
         SceneManager.LoadScene(3);
     }
 
-    public void HardScene()//어려움씬 시작
+    public void HardScene1()//어려움씬 시작
     {
         SceneManager.LoadScene(4);
     }
 
-    public void HowToPlayScene()//튜토리얼씬 시작
+    public void HowToPlayScene1()//튜토리얼씬 시작
     {
         SceneManager.LoadScene(5);
     }
-    
-    public void ExitScene()//프로그램 종료
+
+    public void ExitScene1()//프로그램 종료
     {
         Debug.Log("Quit");
         Application.Quit();
