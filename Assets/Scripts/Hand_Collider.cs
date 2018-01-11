9c27e54c3a52a109686a8838824103277eeb929d
keon kang <kangkk9714@gmail.com>
2018-01-11 10:04:51 +0000

    V.1.51

--- Assets/Scripts/Hand_Collider.cs
+++ Assets/Scripts/Hand_Collider.cs
@@ -11,13 +11,14 @@ public class Hand_Collider : MonoBehaviour
             OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
         Debug.Log(Grap_F);
     }
-    private void OnCollisionEnter(Collision c)
+    private void OnTriggerEnter(Collider c)
     {
         
-        if (c.gameObject.tag=="PH")
+        if (c.gameObject.tag == "PH")
         {
             Debug.Log("ddd");
             c.gameObject.SendMessage("H_ChangeL");
         }
     }
 }
+
