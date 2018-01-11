8bf051666d0dc14d96c417c3ea3389ac7fa2fb2b
keon kang <kangkk9714@gmail.com>
2018-01-11 12:43:34 +0000

    V.1.55

---
+++ Assets/Scripts/Hand_Collider1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand_Collider1 : MonoBehaviour {
+
+    float Grap_F;
+    public GameObject GunR;
+    private void Update()
+    {
+        Grap_F =
+            OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        Debug.Log(Grap_F);
+    }
+    private void OnCollisionStay(Collision c)
+    {
+        Debug.Log(c.transform.name);
+        if (c.gameObject.tag == "Player" && Grap_F >= 0.8)
+        {
+            Debug.Log("ddd");
+            GunR.SendMessage("H_changeR");
+            Destroy(this.gameObject);
+        }
+    }
+}
