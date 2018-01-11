3401ff03d2d5b073967de48090b5d987e6a92e5b
keon kang <kangkk9714@gmail.com>
2018-01-11 06:46:04 +0000

    V.1.46

---
+++ Assets/Scripts/Hand_Collider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand_Collider : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision c)
+    {
+        if(c.gameObject.tag=="PH")
+        {
+            c.gameObject.SendMessage("H_ChangeL");
+        }
+    }
+}
