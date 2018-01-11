9a99fd2022a351e6f65bc5cde52b87d7754091b3
keon kang <kangkk9714@gmail.com>
2018-01-11 14:22:33 +0000

    V.1.61

--- Assets/Scripts/Hand_Collider1.cs
+++ Assets/Scripts/Hand_Collider1.cs
@@ -6,18 +6,17 @@ public class Hand_Collider1 : MonoBehaviour {
 
     float Grap_F;//그랩 입력값
     public GameObject GunR;//오른쪽 홀스터 총
+
     private void Update()
     {
         Grap_F =
-            OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger, OVRInput.Controller.LTouch);
+            OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger, OVRInput.Controller.RTouch);
         Debug.Log(Grap_F);
     }
     private void OnCollisionStay(Collision c)//충돌판정
     {
-        Debug.Log(c.transform.name);
         if (c.gameObject.tag == "Player" && Grap_F >= 0.8)//손이 플레이어충돌하고 그랩인풋값들어왔을때
         {
-            Debug.Log("ddd");
             GunR.SendMessage("H_changeR");
             Destroy(this.gameObject);
         }
