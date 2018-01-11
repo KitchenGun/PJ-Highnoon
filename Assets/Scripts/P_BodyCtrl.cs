dc1e88fb78371842182bfee2feafcf49e01e47ee
keon kang <kangkk9714@gmail.com>
2018-01-11 09:33:39 +0000

    V.1.50

--- Assets/Scripts/P_BodyCtrl.cs
+++ Assets/Scripts/P_BodyCtrl.cs
@@ -8,6 +8,7 @@ public class P_BodyCtrl : MonoBehaviour
     public Transform Body;
     private void Update()
     {
+        Body.transform.position = Head.transform.position+new Vector3(0,-0.5f,0);
         //바라보는 방향을 기준으로 이동하기 위한 몸통 회전
         Vector3 headEuler = Head.localEulerAngles;
         Body.localEulerAngles = new Vector3(0, headEuler.y, 0);
