65d0dcf6d741470f6a1ac7a4d480802ae7776613
keon kang <kangkk9714@gmail.com>
2018-01-15 09:29:52 +0000

    V.2.19

---
+++ Assets/Scripts/LogoLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LogoLight : MonoBehaviour
+{
+    public Light LightLogo;//로고를 비추는 스팟 라이트
+
+    // Use this for initialization
+    void Start()
+    {
+        LightLogo.spotAngle = 0;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+            
+        LightLogo.spotAngle += Time.deltaTime * 30;
+        if(LightLogo.spotAngle>160)
+        {
+            Debug.Log("dd");
+            SceneManager.LoadScene(0);
+        }
+    }
+}
